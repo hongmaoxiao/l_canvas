b8778c793f0be88c95000013d41db967621d19a9
hongmaoxiao <769501294@qq.com>
2022-05-20 01:53:32 +0800

    draw with Path2D and svg path

--- rect/index.js
+++ rect/index.js
@@ -3,9 +3,12 @@ function draw() {
   const canvas = document.getElementById('tutorial')
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d')
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
+    // ctx.fillRect(25, 25, 100, 100);
+    // ctx.clearRect(45, 45, 60, 60);
+    // ctx.strokeRect(50, 50, 50, 50);
+    
+    // 第二种矩形方法
+    // ctx.rect(25, 25, 100, 100)
   }
 }
 
@@ -109,9 +112,40 @@ function drawQuadratic() {
   }
 }
 
+// Path2D
+function drawPath2D() {
+  const canvas = document.getElementById('tutorial')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+
+    const rectangle = new Path2D()
+    rectangle.rect(10, 10, 50, 50)
+
+    const circle = new Path2D()
+    circle.moveTo(125, 35)
+    circle.arc(100, 35, 25, 0, Math.PI * 2)
+
+    ctx.stroke(rectangle)
+    ctx.fill(circle)
+  }
+}
+
+// SVG Path
+function drawSvgPath() {
+  const canvas = document.getElementById('tutorial')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+
+    const p = new Path2D('M10 10 h 80 v 80 h -80 Z')
+    ctx.fill(p)
+  }
+}
+
 // draw()
 // drawTriangle()
 // drawFace()
 // drawTwoTriangle()
 // drawMoreCircle()
-drawQuadratic()
+// drawQuadratic()
+// drawPath2D()
+drawSvgPath()

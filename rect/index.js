ff985a57d98080b0b2c289905e5ff1f2f91d2bcb
hongmaoxiao <769501294@qq.com>
2022-05-19 21:59:23 +0800

    draw face

--- rect/index.js
+++ rect/index.js
@@ -9,8 +9,6 @@ function draw() {
   }
 }
 
-// draw()
-
 // 三角形
 function drawTriangle() {
   const canvas = document.getElementById('tutorial')
@@ -25,4 +23,25 @@ function drawTriangle() {
   }
 }
 
-drawTriangle()
+// 画笑脸
+function drawFace() {
+  const canvas = document.getElementById('tutorial')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true) // 圆
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false) // 口(顺时针)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true) // 左眼
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true) // 右眼
+    ctx.stroke();
+  }
+}
+
+
+// draw()
+// drawTriangle()
+drawFace()

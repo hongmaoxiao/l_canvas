c3a6a6f187db3e11beeab79cd99c904869524a76
hongmaoxiao <769501294@qq.com>
2022-05-20 04:15:12 +0800

    draw lineJoin

--- color/index.js
+++ color/index.js
@@ -140,9 +140,34 @@ function drawLineCap() {
   }
 }
 
+
+// lineJoin
+function drawLineJoin() {
+  const canvas = document.getElementById("tutorial");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    const lineJoin = ['round', 'bevel', 'miter']
+
+    ctx.lineWidth = 10
+    for (let i = 0; i < lineJoin.length; i++) {
+      ctx.lineJoin = lineJoin[i]
+
+      ctx.beginPath()
+      ctx.moveTo(-5, 5 + i * 40)
+      ctx.lineTo(35, 45 + i * 40)
+      ctx.lineTo(75, 5 + i * 40)
+      ctx.lineTo(115, 45 + i * 40)
+      ctx.lineTo(155, 5 + i * 40)
+      ctx.stroke()
+    }
+  }
+}
+
 // draw()
 // drawCircle()
 // drawTransparency()
 // drawRgba()
 // drawLineWidth()
-drawLineCap()
+// drawLineCap()
+drawLineJoin()

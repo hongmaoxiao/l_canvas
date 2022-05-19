000346988c26b7a49d190388cf511fad3b340014
hongmaoxiao <769501294@qq.com>
2022-05-20 02:33:51 +0800

    draw Transparency

--- color/index.js
+++ color/index.js
@@ -1,34 +1,71 @@
 function draw() {
-  const canvas = document.getElementById('tutorial')
+  const canvas = document.getElementById("tutorial");
   if (canvas.getContext) {
-  const ctx = canvas.getContext('2d')
-   for (let i = 0; i < 6; i++) {
-     for (let j = 0; j < 6; j++) {
-       ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ','
-       + Math.floor(255 - 42.5 * j) + ',0)';
+    const ctx = canvas.getContext("2d");
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        ctx.fillStyle =
+          "rgb(" +
+          Math.floor(255 - 42.5 * i) +
+          "," +
+          Math.floor(255 - 42.5 * j) +
+          ",0)";
 
-       ctx.fillRect(j * 25, i * 25, 25, 25);
-     }
-   }
+        ctx.fillRect(j * 25, i * 25, 25, 25);
+      }
+    }
   }
 }
 
 function drawCircle() {
-  const canvas = document.getElementById('tutorial')
+  const canvas = document.getElementById("tutorial");
   if (canvas.getContext) {
-  const ctx = canvas.getContext('2d')
-   for (let i = 0; i < 6; i++) {
-     for (let j = 0; j < 6; j++) {
-       ctx.strokeStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ','
-       + Math.floor(255 - 42.5 * j) + ',0)';
+    const ctx = canvas.getContext("2d");
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        ctx.strokeStyle =
+          "rgb(" +
+          Math.floor(255 - 42.5 * i) +
+          "," +
+          Math.floor(255 - 42.5 * j) +
+          ",0)";
 
-       ctx.beginPath();
-       ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
-       ctx.stroke();
-     }
-   }
+        ctx.beginPath();
+        ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+// 画透明度
+function drawTransparency() {
+  const canvas = document.getElementById("tutorial");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    // 画背景
+    ctx.fillStyle = "#FD0";
+    ctx.fillRect(0, 0, 75, 75);
+    ctx.fillStyle = "#6C0";
+    ctx.fillRect(75, 0, 75, 75);
+    ctx.fillStyle = "#09F";
+    ctx.fillRect(0, 75, 75, 75);
+    ctx.fillStyle = "#F30";
+    ctx.fillRect(75, 75, 75, 75);
+    ctx.fillStyle = "#FFF";
+
+    // 设置透明度值
+    ctx.globalAlpha = 0.2;
+
+    // 画半透明圆
+    for (let i = 0; i < 7; i++) {
+      ctx.beginPath()
+      ctx.arc(75, 75, 10+10*i, 0, Math.PI * 2, true)
+      ctx.fill()
+    }
   }
 }
 
 // draw()
-drawCircle()
+// drawCircle()
+drawTransparency()

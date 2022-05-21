ec989c6bc24f284f32fb413ed79dc6a3ce648be5
hongmaoxiao <769501294@qq.com>
2022-05-22 02:35:19 +0800

    draw MeasureText

--- text/index.js
+++ text/index.js
@@ -1,6 +1,5 @@
 function drawHelloworld() {
   const canvas = document.getElementById('tutorial')
-  console.log('in here now')
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d')
     
@@ -9,4 +8,40 @@ function drawHelloworld() {
   }
 }
 
-drawHelloworld()
+function drawHelloworldStroke() {
+  const canvas = document.getElementById('tutorial')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+    
+    ctx.font = "20px serif"
+    ctx.strokeText("Hello, World!", 10, 50);
+  }
+}
+
+// baseline
+function drawBaseline() {
+  const canvas = document.getElementById('tutorial')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+    
+    ctx.font = "20px serif"
+    ctx.textBaseline = 'bottom';
+    ctx.strokeText("Hello, World!", 0, 50);
+  }
+}
+
+// measureText
+function drawMeasureText() {
+  const canvas = document.getElementById('tutorial')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+    
+    const text = ctx.measureText('foo')
+    console.log('text: ', text)
+  }
+}
+
+// drawHelloworld()
+// drawHelloworldStroke()
+// drawBaseline()
+drawMeasureText()

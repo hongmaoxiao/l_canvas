6cf313e61776766498131d841b354c534995984f
hongmaoxiao <769501294@qq.com>
2022-05-22 03:07:11 +0800

    draw scaling image

--- image/index.js
+++ image/index.js
@@ -20,4 +20,24 @@ function drawImage() {
   }
 }
 
-drawImage()
+function drawScalingImage() {
+  const canvas = document.getElementById('tutorial')
+  console.log('in here now')
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d')
+    
+    const img = new Image()
+    img.onload = function () {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+          ctx.drawImage(img, j*50, i*38, 50, 38)
+        }
+      }
+    }
+
+    img.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
+  }
+}
+
+// drawImage()
+drawScalingImage()
